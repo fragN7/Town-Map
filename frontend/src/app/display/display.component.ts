d821fc842d470df5b05a7fd20fe76bc13efc26da
Alen <132235388+fragN7@users.noreply.github.com>
2023-09-26 21:15:37 +0300

    Gotovo mislim ce sve, fali responsiveness, deployment i database

--- frontend/src/app/display/display.component.ts
+++ frontend/src/app/display/display.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ServiceComponent} from "../service/service.component";
 import {Router} from "@angular/router";
+import {first} from "rxjs";
 
 @Component({
   selector: 'app-display',
@@ -9,14 +10,12 @@ import {Router} from "@angular/router";
 })
 export class DisplayComponent implements OnInit{
 
-  item = {
-    person: null,
-    parents: [],
-    grandParents: [],
-    siblings: [],
-    kids: [],
-    firstCousins: []
-  }
+  person: any = null;
+  parents: any[] = [];
+  grandParents: any[] = [];
+  siblings: any[] = [];
+  kids: any[] = [];
+  firstCousins: any[] = [];
 
   constructor(private service: ServiceComponent, private router: Router) { }
 
@@ -32,7 +31,7 @@ export class DisplayComponent implements OnInit{
   getPerson(name: string){
     this.service.getPerson(name).subscribe(
       (response: any) => {
-        this.item.person = response;
+        this.person = response;
       },
       (error: any) => {
         alert(error.error);
@@ -42,7 +41,7 @@ export class DisplayComponent implements OnInit{
   getParents(name: string){
     this.service.getParents(name).subscribe(
       (response: any) => {
-        this.item.parents = response;
+        this.parents = response;
       },
       (error: any) => {
         alert(error.error);
@@ -52,7 +51,7 @@ export class DisplayComponent implements OnInit{
   getGrandParents(name: string){
     this.service.getGrandParents(name).subscribe(
       (response: any) => {
-        this.item.grandParents = response;
+        this.grandParents = response;
       },
       (error: any) => {
         alert(error.error);
@@ -62,7 +61,7 @@ export class DisplayComponent implements OnInit{
   getSiblings(name: string){
     this.service.getSiblings(name).subscribe(
       (response: any) => {
-        this.item.siblings = response;
+        this.siblings = response;
       },
       (error: any) => {
         alert(error.error);
@@ -72,7 +71,7 @@ export class DisplayComponent implements OnInit{
   getKids(name: string){
     this.service.getKids(name).subscribe(
       (response: any) => {
-        this.item.kids = response;
+        this.kids = response;
       },
       (error: any) => {
         alert(error.error);
@@ -82,11 +81,58 @@ export class DisplayComponent implements OnInit{
   getFirstCousins(name: string){
     this.service.getFirstCousins(name).subscribe(
       (response: any) => {
-        this.item.firstCousins = response;
+        this.firstCousins = response;
       },
       (error: any) => {
         alert(error.error);
       }
     );
   }
+
+  ageCalculator(date: string) {
+    // Split the date string into day, month, and year components
+    const dateParts = date.split('/');
+
+    // Ensure there are exactly 3 parts (day, month, year)
+    if (dateParts.length !== 3) {
+      return null; // Invalid date format
+    }
+
+    const day = parseInt(dateParts[0], 10);
+    const month = parseInt(dateParts[1], 10);
+    const year = parseInt(dateParts[2], 10);
+
+    // Create a Date object from the components
+    const birthDate = new Date(year, month - 1, day); // Month is 0-based
+
+    // Get the current date
+    const currentDate = new Date();
+
+    // Calculate the age
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    // Check if the birthday has occurred this year
+    if (
+      currentDate.getMonth() < birthDate.getMonth() ||
+      (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())
+    ) {
+      age--; // Subtract 1 year if the birthday hasn't occurred yet this year
+    }
+
+    console.log(age);
+    return age;
+  }
+
+
+  redirectPerson(name: string){
+    localStorage.setItem('personName', name);
+    this.router.navigateByUrl(`/${name}`);
+    window.location.reload();
+  }
+
+  redirectToMainScreen(){
+    this.router.navigateByUrl("/");
+  }
+
+  protected readonly first = first;
 }
